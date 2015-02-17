b6548c518821f6be4f3cfe62aa6bf61d07537b8a
braxtonnunnally@gmail.com <braxtonnunnally@gmail.com>
2015-02-16 22:41:54 -0600

    Added program start file

---
+++ adam.braxton.stock/Promp.java
@@ -0,0 +1,24 @@
+class Prompt {
+
+boolean keepBuying;
+Console prompt;
+Bank transaction; 
+Calculator calculator;
+
+   public static void main(String[] args)
+   {
+       prompt = new Console();
+        while(keepBuying) {
+            if(!hasUser){
+               prompt.getName();
+               prompt.getMoney();
+            }
+           prompt.getStockName();
+           prompt.getShares();
+           transaction.handleTransaction();
+           calculator.claculate();
+           prompt.ask();   // ask user to buy more stock
+        }
+   }
+
+}
