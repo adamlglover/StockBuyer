1d4058b7982ca605d6184f18d6e481c0b98c8140
braxtonnunnally@gmail.com <braxtonnunnally@gmail.com>
2015-02-18 23:15:51 -0600

    blahh

---
+++ test/ClassExample.java
@@ -0,0 +1,11 @@
+public class House {
+   int bedrooms = 5;
+   int bathrooms = 2;
+   int kitchens = 1;
+
+   string maxCapacity(){
+      return "The maxumin ammount of people that can live is " + bedrooms;
+   }
+
+  
+}
