80df69b635e18a17b1bf951bd807c6c94a14e109
braxtonnunnally@gmail.com <braxtonnunnally@gmail.com>
2015-02-18 23:53:44 -0600

    kasm

--- test/Lesson3B.java
+++ test/Lesson3B.java
@@ -2,7 +2,7 @@ public class Lesson3 {
 
  // data types
  Object      typex = "";
- String      type0 = "";
+ String      type0 = " ";
  int         type1 = 0;
  boolean     type2 = true;
  byte        type3 = 0;
@@ -13,5 +13,8 @@ public class Lesson3 {
  long 	     type8 = 0;
  double      type9 = 0.0;
  short       type10 = 0;
+ 
+ int un;  // value is 0
+ String f = " ";
 
 }
